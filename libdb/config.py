e295760163c88abd612611857544072187e7c712
Mihai <mihai.croicu@pcr.uu.se>
2021-01-14 01:05:07 +0100

    Config comments

--- libdb/config.py
+++ libdb/config.py
@@ -1,3 +1,6 @@
+# db_url should NEVER point to a real ViEWS database
+# but rather to one created using escwatransfer!
+
 db_url = 'postgres://mihai@localhost:5432/views_api'
 schema = 'pipeline'
 allowed_loas = ('pgm','cm')
